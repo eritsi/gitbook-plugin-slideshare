f46959bbd597e8328fdfedeaebeb0de1266a1ec4
eritsi <eritsi@gmail.com>
2021-11-29 14:51:03 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function slideshare(url, kwargs) {
+  var endpoint = '//noembed.com/embed?';
+  var page = kwargs.page
+
+  if (!!url.length) {
+    endpoint += 'url=' + encodeURIComponent(url);
+    return '<div class="noembed-wrapper" data-url="' + endpoint + '" data-page=' + page + '>' + url + '</div>';
+  }
+
+  return url;
+}
+
+module.exports = {
+  website: {
+    assets: './assets',
+    js: ['scripts.js'],
+    css: ['style.css']
+  },
+  filters: {
+    slideshare: slideshare,
+  }
+};
